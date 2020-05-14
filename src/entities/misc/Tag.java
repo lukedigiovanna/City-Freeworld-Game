81633021b001518a3ebbff3924b1f055a6101d6b
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2020-05-13 22:26:23 -0400

    18 new/updates tiles

---
+++ src/entities/misc/Tag.java
@@ -0,0 +1,18 @@
+package entities.misc;
+
+import entities.Entity;
+import world.Camera;
+
+public class Tag extends Entity {
+	public Tag(String text, float x, float y) {
+		super(x,y,.5f,.5f);
+	}
+	
+	public void update(float dt) {
+		
+	}
+	
+	public void draw(Camera c) {
+		
+	}
+}
