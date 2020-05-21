504a6da5d548a45d2d55d987533776d3c127cf2d
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2020-05-21 02:24:10 -0400

    icons for shop gui

--- src/display/UpdateNotesDisplay.java
+++ src/display/UpdateNotesDisplay.java
@@ -10,31 +10,14 @@ import display.component.Component;
 
 public class UpdateNotesDisplay extends Display {
 	private String[] notes = {
-		"Implemented a command system in the developer console",
-		"    These are only accessible in developer versions not releases",
-		"New NPC logic",
-		"    Can only 'see' the player within a field of view",
-		"    Player can mug NPCs and still their money by pressing 'E'",
-		"    NPC may turn hostile and try to kill the player when mugged",
-		"    NPCs drop their money, if any, when killed",
-		"Some more visual updates:",
-		"    Updated the player GUI, better health display and shows money/xp/heat",
-		"    Pause screen now fades the gameplay",
-		"    Loading screen until the scene loads",
-		"    Camera zooms in on the player at the beginning of a new scene",
-		"Vehicle modifcations",
-		"    Cars travel down the roads now",
-		"    Stop and turn at intersections",
-		"    Vehicle movement now responds to the tile it drives across",
-		"    Vehicles controlled by AI will no longer ram into each other",
-		"        Allows the player to steal vehicles when they stop by pressing F",
-		"Implemented money and XP",
-		"Added end screen when the player dies",
-		"Added saving/loading game saves (not currently implemented)",
-		"Many bug fixes that make the game run much smoother",
-		"    No camera flickering anymore - allows higher FPS",
-		"    Hitboxes stay fixed to objects and function correctly",
-		"Other minor tweaks and features"
+		"Added rotatable entity objects",
+		"Many new tiles",
+		"Added text tags to mark places in the world",
+		"Updates input control to control keyboard inputs based on program state",
+		"Implemented a world sound engine",
+		"    Dynamically updates sound based on distance between source and receiver",
+		"Added shops to the game which things can be purchased from",
+		"    Press 'q' near an interactable object to open a shop GUI"
 	};
 	
 	private Button backButton = new BackButton(0.025f,0.025f);
